71ffaa8aa72bc9ee31961f06c62cf531f54b9ed1
José García Palacio <enjoyjos@hotmail.com>
2020-03-19 18:20:26 +0100

    Aplicación con rutas3

--- joysweets/src/app/app-routing.module.ts
+++ joysweets/src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { RecetaComponent } from './pages/receta/receta.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'blog', component: BlogComponent},
+  {path: 'receta/', component: RecetaComponent},
+  {path: 'contacto', component: ContactoComponent},
+  {path: '**',  pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash:true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- joysweets/src/app/app.module.ts
+++ joysweets/src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { RecetaComponent } from './pages/receta/receta.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    BlogComponent,
+    RecetaComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
